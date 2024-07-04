e89b7e4533a07cd2afa9a324c2f2d073f6b96231
Navneeth <navneeth@combatrobotics.in>
2024-07-05 01:54:57 +0530

    modified cockpit.sh file

--- cockpit.sh
+++ cockpit.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 bun run dev --host &
+python3 scripts/ping_display.py &
+python3 scripts/home_navig.py &
 sleep 3
-chromium-browser --start-fullscreen http://localhost:5173/#/
+chromium-browser --start-fullscreen http://localhost:5173/#/ 
 
