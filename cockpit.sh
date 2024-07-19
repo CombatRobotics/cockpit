865c0d26472282f167d7495a1bd427b04d965bd7
Harsh Davda <harsh.davda11@outlook.com>
2024-07-19 12:03:14 +0530

    removed chromium as ui app

--- cockpit.sh
+++ cockpit.sh
@@ -3,5 +3,5 @@ bun run dev --host &
 python3 scripts/ping_display.py &
 python3 scripts/home_navig.py &
 sleep 3
-chromium-browser --start-fullscreen http://localhost:5173/#/ 
+# chromium-browser --start-fullscreen http://localhost:5173/#/ 
 
